79f340c5bb148c5ec9b3511d40e9e477d45a662d
youkun119-netizen <youkun119@gmail.com>
2026-07-17 14:09:07 +0900

    [프로그래머스] L0 - 두 수의 곱 구하기 (#6)

--- programmers/L0_기초입문/입문/006_두_수의_곱_구하기.py
+++ programmers/L0_기초입문/입문/006_두_수의_곱_구하기.py
@@ -3,8 +3,12 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120804
 # 알고리즘: 기초
 # 작성자: 학생
-# 작성일: 2026. 07. 17. 13:57:34
+# 작성일: 2026. 07. 17. 14:09:06
 
 def solution(num1, num2):
-    if (0 <= num1 <= 100) and (0 <= num2 <= 100):
-        return num1*num2
+    i = 0
+    answer = 0
+    while i < num2:
+        answer += num1
+        i += 1
+    return answer
