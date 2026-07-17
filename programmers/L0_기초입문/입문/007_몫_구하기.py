91aeb17e731f7d6f130c0dc1061c3822cf08b9b7
youkun119-netizen <youkun119@gmail.com>
2026-07-17 14:15:14 +0900

    [프로그래머스] L0 - 몫 구하기 (#7)

---
+++ programmers/L0_기초입문/입문/007_몫_구하기.py
@@ -0,0 +1,10 @@
+# 몫 구하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120805
+# 알고리즘: 기초
+# 작성자: 학생
+# 작성일: 2026. 07. 17. 14:15:13
+
+def solution(num1, num2)->int:
+    if (0 < num1 <= 100) and (0 < num2 <= 100):
+        return num1//num2
