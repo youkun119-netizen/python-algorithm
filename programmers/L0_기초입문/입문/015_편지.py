44394dbf35030195370de7cf89123b0734522b43
youkun119-netizen <youkun119@gmail.com>
2026-07-17 16:05:09 +0900

    [프로그래머스] L0 - 편지 (#15)

---
+++ programmers/L0_기초입문/입문/015_편지.py
@@ -0,0 +1,9 @@
+# 편지
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120898
+# 알고리즘: 기초
+# 작성자: 학생
+# 작성일: 2026. 07. 17. 16:05:09
+
+def solution(message):
+    return len(message) * 2
