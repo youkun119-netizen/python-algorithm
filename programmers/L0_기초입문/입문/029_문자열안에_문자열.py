0fd950f123e93c2041a4e6bbd0d4a3202ed62560
youkun119-netizen <youkun119@gmail.com>
2026-07-17 21:28:27 +0900

    [프로그래머스] L0 - 문자열안에 문자열 (#29)

---
+++ programmers/L0_기초입문/입문/029_문자열안에_문자열.py
@@ -0,0 +1,12 @@
+# 문자열안에 문자열
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120908
+# 알고리즘: 기초
+# 작성자: 학생
+# 작성일: 2026. 07. 17. 21:28:25
+
+def solution(str1, str2):
+    if str2 in str1:
+        return 1
+    else:
+        return 2
