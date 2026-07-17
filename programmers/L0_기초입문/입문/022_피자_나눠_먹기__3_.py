2c820d466d9bd55a38d4800fe9c0445f3fce171a
youkun119-netizen <youkun119@gmail.com>
2026-07-17 17:40:00 +0900

    [프로그래머스] L0 - 피자 나눠 먹기 (3) (#22)

---
+++ programmers/L0_기초입문/입문/022_피자_나눠_먹기__3_.py
@@ -0,0 +1,9 @@
+# 피자 나눠 먹기 (3)
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120816
+# 알고리즘: 기초
+# 작성자: 학생
+# 작성일: 2026. 07. 17. 17:39:58
+
+def solution(slice, n):
+    return (n-1)//slice + 1
