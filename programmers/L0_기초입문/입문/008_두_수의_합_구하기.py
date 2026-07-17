152df549ba32fef2903ce575eab8ba18a15ec68a
youkun119-netizen <youkun119@gmail.com>
2026-07-17 14:22:03 +0900

    [프로그래머스] L0 - 두 수의 합 구하기 (#8)

---
+++ programmers/L0_기초입문/입문/008_두_수의_합_구하기.py
@@ -0,0 +1,10 @@
+# 두 수의 합 구하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120802
+# 알고리즘: 기초
+# 작성자: 학생
+# 작성일: 2026. 07. 17. 14:22:01
+
+def solution(num1, num2):
+    if (-50000 <= num1 <= 50000) and (-50000 <= num2 <= 50000):
+        return num1 + num2
