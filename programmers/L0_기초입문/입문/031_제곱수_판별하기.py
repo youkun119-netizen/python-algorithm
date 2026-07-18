851b0443d66545adb966b05b818967239b0ae220
youkun119-netizen <youkun119@gmail.com>
2026-07-18 16:11:13 +0900

    [프로그래머스] L0 - 제곱수 판별하기 (#31)

--- programmers/L0_기초입문/입문/031_제곱수_판별하기.py
+++ programmers/L0_기초입문/입문/031_제곱수_판별하기.py
@@ -3,10 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120909
 # 알고리즘: 기초
 # 작성자: 학생
-# 작성일: 2026. 07. 18. 16:06:23
+# 작성일: 2026. 07. 18. 16:11:13
 
 def solution(n):
-    if (n**0.5) % 1 == 0:
-        return 1
-    else:
-        return 2
+    return 1 if (n**0.5).is_integer() else 2 
