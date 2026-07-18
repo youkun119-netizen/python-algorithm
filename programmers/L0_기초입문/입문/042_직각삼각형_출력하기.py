8d28ba05bf9e8d8aee5f306e91beb55cfc3431c8
youkun119-netizen <youkun119@gmail.com>
2026-07-18 21:03:04 +0900

    [프로그래머스] L0 - 직각삼각형 출력하기 (#42)

---
+++ programmers/L0_기초입문/입문/042_직각삼각형_출력하기.py
@@ -0,0 +1,11 @@
+# 직각삼각형 출력하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120823
+# 알고리즘: 기초
+# 작성자: 학생
+# 작성일: 2026. 07. 18. 21:03:03
+
+n = int(input())
+
+for i in range(1, n+1):
+    print("*"*i)
