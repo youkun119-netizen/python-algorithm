a6cc71ea85ba7daa646f60ff347bdfbba7159238
youkun119-netizen <youkun119@gmail.com>
2026-07-18 19:50:19 +0900

    [프로그래머스] L0 - 짝수는 싫어요 (#40)

--- programmers/L0_기초입문/입문/040_짝수는_싫어요.py
+++ programmers/L0_기초입문/입문/040_짝수는_싫어요.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120813
 # 알고리즘: 기초
 # 작성자: 학생
-# 작성일: 2026. 07. 18. 19:49:08
+# 작성일: 2026. 07. 18. 19:50:18
 
 def solution(n):
-    return [i for i in range(1, n+1) if i % 2 == 1]
+    return list(range(1, n+1, 2))
