069e4a868908f59bcdfde5ffc64e003ab1d42c75
youkun119-netizen <youkun119@gmail.com>
2026-07-19 14:33:53 +0900

    [프로그래머스] L0 - 암호 해독 (#48)

--- programmers/L0_기초입문/입문/048_암호_해독.py
+++ programmers/L0_기초입문/입문/048_암호_해독.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120892
 # 알고리즘: 기초
 # 작성자: 학생
-# 작성일: 2026. 07. 19. 14:27:26
+# 작성일: 2026. 07. 19. 14:33:53
 
 def solution(cipher, code):
-    return cipher[code -1:: code]
+    return cipher[code - 1:: code]
