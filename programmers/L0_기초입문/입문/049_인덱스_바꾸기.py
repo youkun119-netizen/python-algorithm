cd0ee46c1f6654446dd52d1994ad3038b2318481
youkun119-netizen <youkun119@gmail.com>
2026-07-19 15:13:30 +0900

    [프로그래머스] L0 - 인덱스 바꾸기 (#49)

---
+++ programmers/L0_기초입문/입문/049_인덱스_바꾸기.py
@@ -0,0 +1,11 @@
+# 인덱스 바꾸기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120895
+# 알고리즘: 기초
+# 작성자: 학생
+# 작성일: 2026. 07. 19. 15:13:30
+
+def solution(my_string, num1, num2):
+    s = list(my_string)
+    s[num1], s[num2] = s[num2], s[num1]
+    return ''.join(s)
