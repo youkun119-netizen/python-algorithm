0870fa80a390b9b91ae92dcc69aff62fb9b77dc4
youkun119-netizen <youkun119@gmail.com>
2026-07-19 15:43:53 +0900

    [프로그래머스] L0 - 문자열 정렬하기 (2) (#52)

---
+++ programmers/L0_기초입문/입문/052_문자열_정렬하기__2_.py
@@ -0,0 +1,9 @@
+# 문자열 정렬하기 (2)
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120911
+# 알고리즘: 기초
+# 작성자: 학생
+# 작성일: 2026. 07. 19. 15:43:52
+
+def solution(my_string):
+    return ''.join(sorted(my_string.lower()))
