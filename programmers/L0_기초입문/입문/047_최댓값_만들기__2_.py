1866d36c5f5efb13dc363d87f97f4df1333db06d
youkun119-netizen <youkun119@gmail.com>
2026-07-19 14:18:29 +0900

    [프로그래머스] L0 - 최댓값 만들기 (2) (#47)

--- programmers/L0_기초입문/입문/047_최댓값_만들기__2_.py
+++ programmers/L0_기초입문/입문/047_최댓값_만들기__2_.py
@@ -3,9 +3,13 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120862
 # 알고리즘: 기초
 # 작성자: 학생
-# 작성일: 2026. 07. 19. 14:09:17
+# 작성일: 2026. 07. 19. 14:18:29
+
+from itertools import combinations as comb
 
 def solution(numbers):
-    numbers.sort()
-    return max(numbers[0]*numbers[1], numbers[-1]* numbers[-2])
+    an_list = []
+    for i, j in comb(numbers, 2):
+        an_list.append(i*j)
         
+    return max(an_list)
