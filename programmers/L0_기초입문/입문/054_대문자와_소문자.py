2716fafed9a0c29719f0e69c484b5988602c07d9
youkun119-netizen <youkun119@gmail.com>
2026-07-19 16:41:40 +0900

    [프로그래머스] L0 - 대문자와 소문자 (#54)

---
+++ programmers/L0_기초입문/입문/054_대문자와_소문자.py
@@ -0,0 +1,9 @@
+# 대문자와 소문자
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120893
+# 알고리즘: 기초
+# 작성자: 학생
+# 작성일: 2026. 07. 19. 16:41:40
+
+def solution(my_string):
+    return my_string.swapcase()
