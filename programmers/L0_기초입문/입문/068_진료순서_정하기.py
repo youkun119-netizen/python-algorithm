2731b947f83c66749ee356cbe4338be33aa0094b
youkun119-netizen <youkun119@gmail.com>
2026-07-25 21:34:00 +0900

    [프로그래머스] L0 - 진료순서 정하기 (#68)

--- programmers/L0_기초입문/입문/068_진료순서_정하기.py
+++ programmers/L0_기초입문/입문/068_진료순서_정하기.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120835
 # 알고리즘: 기초
 # 작성자: 학생
-# 작성일: 2026. 07. 25. 12:12:20
+# 작성일: 2026. 07. 25. 12:43:00
 
 def solution(emergency):
     s = sorted(emergency, reverse = True)
