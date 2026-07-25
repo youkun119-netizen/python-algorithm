b3540aa281fbb17535df9621a44c47c142dca8a4
youkun119-netizen <youkun119@gmail.com>
2026-07-25 22:00:15 +0900

    [프로그래머스] L0 - 가까운 수 (#69)

--- programmers/L0_기초입문/입문/069_가까운_수.py
+++ programmers/L0_기초입문/입문/069_가까운_수.py
@@ -3,10 +3,10 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120890
 # 알고리즘: 기초
 # 작성자: 학생
-# 작성일: 2026. 07. 25. 13:08:34
+# 작성일: 2026. 07. 25. 13:09:15
 
 def solution(array, n):
     array.sort()
-    answer = min(array, key = lambda x : abs(x - n))
+    answer = min(array, key = lambda x : (abs(x - n), x))
     return answer
     
