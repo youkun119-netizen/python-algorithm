1003b414e60e29c03d705d42d9ab84a4c83fe375
youkun119-netizen <youkun119@gmail.com>
2026-07-25 20:56:19 +0900

    [프로그래머스] L0 - 7의 개수 (#67)

---
+++ programmers/L0_기초입문/입문/067_7의_개수.py
@@ -0,0 +1,9 @@
+# 7의 개수
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120912
+# 알고리즘: 기초
+# 작성자: 학생
+# 작성일: 2026. 07. 25. 12:05:19
+
+def solution(array):
+    return str(array).count("7")
