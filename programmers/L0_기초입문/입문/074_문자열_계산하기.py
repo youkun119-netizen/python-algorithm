0f4071351387d776885218e70f2e51e763241361
youkun119-netizen <youkun119@gmail.com>
2026-08-08 18:24:24 +0900

    [프로그래머스] L0 - 문자열 계산하기 (#74)

---
+++ programmers/L0_기초입문/입문/074_문자열_계산하기.py
@@ -0,0 +1,10 @@
+# 문자열 계산하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120902
+# 알고리즘: 기초
+# 작성자: 학생
+# 작성일: 2026. 08. 08. 18:24:23
+
+def solution(my_string):
+    answer = eval(my_string)
+    return answer
