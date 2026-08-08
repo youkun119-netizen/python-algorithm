c77b42980f86c0b2be810aee65e3845b8e962c97
youkun119-netizen <youkun119@gmail.com>
2026-08-08 19:11:51 +0900

    [프로그래머스] L0 - 이진수 더하기 (#76)

--- programmers/L0_기초입문/입문/076_이진수_더하기.py
+++ programmers/L0_기초입문/입문/076_이진수_더하기.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120885
 # 알고리즘: 기초
 # 작성자: 학생
-# 작성일: 2026. 08. 08. 19:11:27
+# 작성일: 2026. 08. 08. 19:11:50
 
 def solution(bin1, bin2):
     answer = int(bin1, 2) + int(bin2, 2)
